cd34d1f1b9313140e7f59836b5637bd3b89fc0ea
Administrator <Administrator@WIN-9G533O6HT42>
2018-11-13 18:53:06 +0800

    aa

--- src/main/java/controller/Duoxiancheng_controller.java
+++ src/main/java/controller/Duoxiancheng_controller.java
@@ -2,10 +2,12 @@ package controller;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -30,14 +32,15 @@ public class Duoxiancheng_controller {
 
 	@Resource(name = "Docmentory_ServiceImpl")
 	Docmentory_Service service;
-	MD5 md5 = new MD5();
-
+	MD5 md5 = new MD5();//用于给文件名加密
+	InetAddress ia=null;//用于获取本机ip
 	@RequestMapping("/upload")
 	public @ResponseBody JsonInfo add(HttpServletRequest request, HttpServletResponse response)
 			throws IllegalStateException, IOException {
 		Map<String, Object> resultMap = new HashMap<String, Object>();
-		// 解析请求中的数据
-		MultipartHttpServletRequest mpRequest = (MultipartHttpServletRequest) request;
+	
+		MultipartHttpServletRequest mpRequest = (MultipartHttpServletRequest) request;	// 解析请求中的数据
+		
 		MultipartFile file = mpRequest.getFile("file");// 文件是MultipartFile类型的
 
 		Docmentory doc = new Docmentory();
@@ -45,35 +48,36 @@ public class Duoxiancheng_controller {
 		MD5 md5 = new MD5();
 
 		Date date = new Date();// 设置当前时间
+		
+		String uuid = UUID.randomUUID().toString().replace("-", "").toLowerCase();//生成uuid
+		
 		String lastname = file.getOriginalFilename(); // 获取文件名称
 
-		String nowname = md5.MD5(lastname).substring(0, 8);// 对文件名加密并且截取前八位
-
-		SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmssSSS");
+		SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
 
 		String datime = format.format(date);// 获取当前时间的字符串
 		
-		String day = datime.substring(0, 8);
+		String nowname = md5.MD5(uuid).substring(0, 8);// 对文件名加密并且截取前八位
 		
-		String hour = datime.substring(8, 10);
+		String day = datime.substring(0, 8);//获取当天
+		
+		String hour = datime.substring(8, 10);//获取当前时间的小时
 
-		String time = datime.substring(14,17);
-		
 		doc.setLastname(lastname);
 
 		doc.setCreatetime(date.getTime());
-
-		String file_name = md5.MD5(datime+lastname).substring(0, 8);
 		
-		String downloadpath = "http://192.168.28.101:8080/upload/upload/" + day + "/" + hour + "/" + file_name
-				+ lastname.substring(lastname.lastIndexOf("."), lastname.length());// 存储到数据库路径
+        String localip=ia.getLocalHost().getHostAddress();//获取本机ip
+        
+        String filename = nowname+ lastname.substring(lastname.lastIndexOf("."), lastname.length());
+        
+		String downloadpath = "http://"+localip+":8080/upload/upload/" + day + "/" + hour + "/" + filename;// 存储到数据库路径
+		
 		String path = request.getRealPath("/upload/"+day+"/"+hour);// 设置上传路径
-		
-		String filename = file_name+ lastname.substring(lastname.lastIndexOf("."), lastname.length());
 		
 		doc.setPath(downloadpath);
 		
-		doc.setNowname(filename);
+		doc.setNowname(nowname);
 		
 		// 将MultipartFile文件转换为file文件
 	    long size_limit =	file.getSize();// 获取文件大小
@@ -84,11 +88,11 @@ public class Duoxiancheng_controller {
 		
 		gongjulei  gg = new gongjulei(file,filename,path);
 		
-		new Thread(gg, "线程"+time).start(); // 启动一个线程 上传文件
+		new Thread(gg, "线程"+uuid).start(); // 启动一个线程 上传文件
 		
-		System.out.println(downloadpath);// 输出存储到数据库的路径
+		//System.out.println(downloadpath);// 输出存储到数据库的路径
 
-		System.out.println(path);// 输出存储到服务器的上级路径
+		//System.out.println(path);// 输出存储到服务器的上级路径
 
 		return new JsonInfo(1, downloadpath);
 	}
